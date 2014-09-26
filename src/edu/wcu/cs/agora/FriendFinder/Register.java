893f3cbaf16b2412d21af9b8aca06be7f2f296b3
tallendev <tallendev@gmail.com>
2014-09-26 17:31:13 -0400

    Created/starting Register Page, minor changes to login.

--- src/edu/wcu/cs/agora/FriendFinder/Register.java
+++ src/edu/wcu/cs/agora/FriendFinder/Register.java
@@ -3,13 +3,13 @@ package edu.wcu.cs.agora.FriendFinder;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Login extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+/**
+ * Created by tyler on 9/26/14.
+ */
+public class Register extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
     }
 }
