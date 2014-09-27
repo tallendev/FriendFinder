4da7a347da9242341d3e0a6012df764e9eaf06e3
tallendev <tallendev@gmail.com>
2014-09-26 22:35:15 -0400

    Privacy Settings Draft

--- src/edu/wcu/cs/agora/FriendFinder/Privacy.java
+++ src/edu/wcu/cs/agora/FriendFinder/Privacy.java
@@ -1,14 +1,15 @@
+package edu.wcu.cs.agora.FriendFinder;
+
 import android.app.Activity;
 import android.os.Bundle;
-import edu.wcu.cs.agora.FriendFinder.R;
 
 /**
  * Created by tyler on 9/27/14.
  */
-public class Settings extends Activity {
+public class Privacy extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.privacy_settings);
     }
 }
