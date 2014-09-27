9652aa1f176ca4b003a71e55f006f00619c6770a
tallendev <tallendev@gmail.com>
2014-09-27 17:07:11 -0400

    Events page with filler text. Events actual information will have to be created dynamically within hte app code.

---
+++ src/edu/wcu/cs/agora/FriendFinder/EventsPage.java
@@ -0,0 +1,15 @@
+package edu.wcu.cs.agora.FriendFinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tyler on 9/27/14.
+ */
+public class EventsPage extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eventspage);
+    }
+}
