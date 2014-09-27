fd7129cae715c89c275aaccbff3d09efd7fe5451
tallendev <tallendev@gmail.com>
2014-09-26 22:09:52 -0400

    Finished Register, Settings xml.

---
+++ src/Settings.java
@@ -0,0 +1,14 @@
+import android.app.Activity;
+import android.os.Bundle;
+import edu.wcu.cs.agora.FriendFinder.R;
+
+/**
+ * Created by tyler on 9/27/14.
+ */
+public class Settings extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
