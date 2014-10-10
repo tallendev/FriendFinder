fed3945cb41bd0c9e72f62f1dbe4166141743662
tallendev <tallendev@gmail.com>
2014-10-10 00:46:23 -0400

    Started server implementation.

--- src/edu/wcu/cs/agora/FriendFinderServer/RequestServer.java
+++ src/edu/wcu/cs/agora/FriendFinderServer/RequestServer.java
@@ -1,8 +1,43 @@
 package edu.wcu.cs.agora.FriendFinderServer;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
 /**
- *
+ * TODO: Finish me
  */
 public class RequestServer
 {
+    public static final int TIMEOUT = 5000;
+
+    private ServerSocket serverSocket;
+    private int port;
+
+    public RequestServer(int port) throws IOException
+    {
+        this.port = port;
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void listen() throws IOException
+    {
+        try (Socket client = serverSocket.accept();
+             Scanner in = new Scanner(client.getInputStream());
+             OutputStream out = client.getOutputStream())
+        {
+            client.setSoTimeout(TIMEOUT);
+            Request request = Request.requestBuilder(in);
+            request.getResponse();
+        }
+        catch (SocketException se)
+        {
+
+        }
+
+    }
+
 }
---
+++ src/edu/wcu/cs/agora/FriendFinderServer/Response.java
@@ -0,0 +1,10 @@
+package edu.wcu.cs.agora.FriendFinderServer;
+
+/**
+ * Created by tyler on 10/10/14.
+ *
+ * TODO: implement me
+ */
+public class Response
+{
+}
