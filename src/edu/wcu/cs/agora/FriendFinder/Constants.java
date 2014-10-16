d3dabbffb1c62d80fa89ada4dda5f3eaa1d0b86b
kdana <kdana3@gmail.com>
2014-10-16 14:48:40 -0400

    No re-rendering on screen rotation

---
+++ src/edu/wcu/cs/agora/FriendFinder/Constants.java
@@ -0,0 +1,8 @@
+package edu.wcu.cs.agora.FriendFinder;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Constants {
+    public static final LatLng CULLOWHEE = new LatLng(35.3113624, -83.1819903);
+    public static final int MAP_ZOOM = 13;
+}
--- src/edu/wcu/cs/agora/FriendFinder/Map.java
+++ src/edu/wcu/cs/agora/FriendFinder/Map.java
@@ -1,27 +1,32 @@
 package edu.wcu.cs.agora.FriendFinder;
 
-import android.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
  * Tyler Allen
+ * Karen Dana
  * 09/29/2014
  *
  * Code for functionality on the log in page.
  */
-public class Map extends Fragment
-{
+public class Map extends Activity {
 
-    /**
-     * Currently the default fragment onCreateView.
-     */
+    private GoogleMap map;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.map, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+        // Get the map fragment
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.layout.map)).getMap();
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Constants.CULLOWHEE, Constants.MAP_ZOOM));
     }
 }
