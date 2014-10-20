c3bb79d9822b17befc53340718a01a3940503690
Karen Dana <kdana3@gmail.com>
2014-10-20 13:23:42 -0400

    Fixed register layout, added volley

--- src/edu/wcu/cs/agora/FriendFinder/Register.java
+++ src/edu/wcu/cs/agora/FriendFinder/Register.java
@@ -2,14 +2,28 @@ package edu.wcu.cs.agora.FriendFinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by tyler on 9/26/14.
  */
-public class Register extends Activity {
+
+public class Register extends Activity implements View.OnClickListener {
+
+    Button signUpButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        //Get the button from the layout
+        signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //finish here
     }
 }
