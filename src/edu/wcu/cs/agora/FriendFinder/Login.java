c95111c0c1ca1c66ddc2d2e8d74739413abdfcbd
Karen Dana <kdana3@gmail.com>
2014-10-20 09:13:01 -0400

    added to network handler

--- src/edu/wcu/cs/agora/FriendFinder/Login.java
+++ src/edu/wcu/cs/agora/FriendFinder/Login.java
@@ -115,7 +115,8 @@ public class Login extends Activity implements View.OnClickListener
             String password = String.valueOf(((EditText) findViewById(R.id.pass)).getText());
 
             //send to server
-
+            NetworkHandler handler = new NetworkHandler();
+            handler.send("user=" + username + " pass=" + password);
 
         } else {
             Intent intent = new Intent(this, Register.class);
--- src/edu/wcu/cs/agora/FriendFinder/NetworkHandler.java
+++ src/edu/wcu/cs/agora/FriendFinder/NetworkHandler.java
@@ -1,8 +1,11 @@
 package edu.wcu.cs.agora.FriendFinder;
 
 /**
+ * Handles the data sent to and received from the server.
+ *
  * Created by Karen on 10/20/2014.
  */
 public class NetworkHandler {
+
 
 }
