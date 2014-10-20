6bef4c74058c9f0771eb0d708e3fdadb4648767b
Karen Dana <kdana3@gmail.com>
2014-10-20 16:26:07 -0400

    updated network handler

--- src/edu/wcu/cs/agora/FriendFinder/Constants.java
+++ src/edu/wcu/cs/agora/FriendFinder/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final int MAP_ZOOM = 13;
     public static final String SETTINGS = "Settings";
     public static final String USERNAME = "username";
+    public static final String URL = "https://www.trantracker.com:1337";
 }
--- src/edu/wcu/cs/agora/FriendFinder/NetworkHandler.java
+++ src/edu/wcu/cs/agora/FriendFinder/NetworkHandler.java
@@ -2,11 +2,17 @@ package edu.wcu.cs.agora.FriendFinder;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 /**
  * Handles the data sent to and received from the server.
+ *
+ * Reference: Android docs simple request using Volley example
  *
  * Created by Karen on 10/20/2014.
  */
@@ -14,6 +20,24 @@ public class NetworkHandler {
 
     public void send(Context context, String message) {
         RequestQueue queue = Volley.newRequestQueue(context);
+
+        //TODO: replace when figure out the server
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constants.URL,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response) {
+                        String responseStr = (String) response;
+                        // What happens with response.
+
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // What happens when error
+            }
+        });
 
     }
 
